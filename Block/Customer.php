f814828f9e5873e7acf02f5096de6b3125b84f11
Luis <luis@tezus.com.br>
2021-06-23 18:08:24 -0300

    Fix: Atualizado para não ter problema com modulo SystemCode

--- Block/Customer.php
+++ Block/Customer.php
@@ -6,38 +6,46 @@ use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 
-class Customer extends Template {
+class Customer extends Template
+{
 
-  /** @var ScopeConfigInterface */
-  protected $_scopeConfig;
+	/** @var ScopeConfigInterface */
+	protected $_scopeConfig;
 
-  public function __construct(Context $context, ScopeConfigInterface $scopeConfig) {
-    $this->setTemplate('Tezus_AutoComplete::customer.phtml');
-    $this->_scopeConfig = $scopeConfig;
-    parent::__construct($context);
-  }
+	public function __construct(Context $context, ScopeConfigInterface $scopeConfig)
+	{
+		$this->setTemplate('Tezus_AutoComplete::customer.phtml');
+		$this->_scopeConfig = $scopeConfig;
+		parent::__construct($context);
+	}
 
-  public function getLoadPageTime() {
-    return $this->_scopeConfig->getValue('tezus_autocomplete/settings/load_page_time');
-  }
+	public function getLoadPageTime()
+	{
+		return $this->_scopeConfig->getValue('tezus_autocomplete/settings/load_page_time');
+	}
 
-  public function getCustomState() {
-    return $this->_scopeConfig->getValue('tezus_autocomplete/custom_fields_customer/custom_state');
-  }
+	public function getCustomState()
+	{
+		return $this->_scopeConfig->getValue('tezus_autocomplete/custom_fields_customer/custom_state');
+	}
 
-  public function getCustomCity() {
-    return $this->_scopeConfig->getValue('tezus_autocomplete/custom_fields_customer/custom_city');
-  }
+	public function getCustomCity()
+	{
+		return $this->_scopeConfig->getValue('tezus_autocomplete/custom_fields_customer/custom_city');
+	}
 
-  public function getCustomStreet() {
-    return $this->_scopeConfig->getValue('tezus_autocomplete/custom_fields_customer/custom_street');
-  }
+	public function getCustomStreet()
+	{
+		return $this->_scopeConfig->getValue('tezus_autocomplete/custom_fields_customer/custom_street');
+	}
 
-  public function getCustomQuarter() {
-    return $this->_scopeConfig->getValue('tezus_autocomplete/custom_fields_customer/custom_quarter');
-  }
+	public function getCustomQuarter()
+	{
+		return $this->_scopeConfig->getValue('tezus_autocomplete/custom_fields_customer/custom_quarter');
+	}
 
-  public function getCustomAdditionalInfo() {
-    return $this->_scopeConfig->getValue('tezus_autocomplete/custom_fields_customer/custom_additional_info');
-  }
+	public function getCustomAdditionalInfo()
+	{
+		return $this->_scopeConfig->getValue('tezus_autocomplete/custom_fields_customer/custom_additional_info');
+	}
 }
--- Model/Checkout/LayoutProcessorPlugin.php
+++ Model/Checkout/LayoutProcessorPlugin.php
@@ -2,68 +2,70 @@
 
 namespace Tezus\AutoComplete\Model\Checkout;
 
-class LayoutProcessorPlugin {
-  public function aroundProcess(
-    \Magento\Checkout\Block\Checkout\LayoutProcessor $subject,
-    \Closure $proceed,
-    array $jsLayout
-  ) {
-    $jsLayoutResult = $proceed($jsLayout);
+use \Magento\Checkout\Block\Checkout\LayoutProcessor;
 
-    $shippingAddress = &$jsLayoutResult['components']['checkout']['children']['steps']['children']['shipping-step']['children']['shippingAddress']['children']['shipping-address-fieldset']['children'];
+class LayoutProcessorPlugin
+{
+	public function aroundProcess(
+		LayoutProcessor $subject,
+		\Closure $proceed,
+		array $jsLayout
+	) {
+		$jsLayoutResult = $proceed($jsLayout);
 
-    unset($shippingAddress['street']['label']);
+		$shippingAddress = &$jsLayoutResult['components']['checkout']['children']['steps']['children']['shipping-step']['children']['shippingAddress']['children']['shipping-address-fieldset']['children'];
 
-    // Shipping fields street labels
-    $shippingAddress['street']['children']['0']['label'] = __('Endereço *');
-    $shippingAddress['street']['children']['1']['label'] = __('Número *');
-    $shippingAddress['street']['children']['2']['label'] = __('Complemento');
-    $shippingAddress['street']['children']['3']['label'] = __('Bairro *');
-    $shippingAddress['postcode']['sortOrder'] = 1;
+		unset($shippingAddress['street']['label']);
 
-    //Validation fields street labels
-    $shippingAddress['street']['children']['0']['validation'] = ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255];
-    $shippingAddress['street']['children']['1']['validation'] = ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255];
-    $shippingAddress['street']['children']['3']['validation'] = ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255];
+		// Shipping fields street labels
+		$shippingAddress['street']['children']['0']['label'] = __('Endereço');
+		$shippingAddress['street']['children']['1']['label'] = __('Número');
+		$shippingAddress['street']['children']['2']['label'] = __('Complemento');
+		$shippingAddress['street']['children']['3']['label'] = __('Bairro');
+		$shippingAddress['postcode']['sortOrder'] = 1;
 
-    return $jsLayoutResult;
-  }
+		//Validation fields street labels
+		$shippingAddress['street']['children']['0']['validation'] = ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255];
+		$shippingAddress['street']['children']['1']['validation'] = ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255];
+		$shippingAddress['street']['children']['3']['validation'] = ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255];
 
-  /**
-   * @param \Magento\Checkout\Block\Checkout\LayoutProcessor $subject
-   * @param array $jsLayout
-   * @return array
-   */
+		return $jsLayoutResult;
+	}
 
-  public function afterProcess(
-    \Magento\Checkout\Block\Checkout\LayoutProcessor $subject,
-    array $jsLayout
-  ) {
+	/**
+	 * @param LayoutProcessor $subject
+	 * @param array $jsLayout
+	 * @return array
+	 */
 
-    // Change billing field labels for every payment method
-    $paymentForms = $jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']['payment']['children']['payments-list']['children'];
+	public function afterProcess(
+		LayoutProcessor $subject,
+		array $jsLayout
+	) {
+		// Change billing field labels for every payment method
+		$paymentForms = $jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']['payment']['children']['payments-list']['children'];
 
-    foreach ($paymentForms as $paymentMethodForm => $paymentMethodValue) {
+		foreach ($paymentForms as $paymentMethodForm => $paymentMethodValue) {
 
-      $paymentMethodCode = str_replace('-form', '', $paymentMethodForm);
+			$paymentMethodCode = str_replace('-form', '', $paymentMethodForm);
 
-      $billingAddress = &$jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']['payment']['children']['payments-list']['children'][$paymentMethodCode . '-form']['children']['form-fields']['children'];
+			$billingAddress = &$jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']['payment']['children']['payments-list']['children'][$paymentMethodCode . '-form']['children']['form-fields']['children'];
 
-      if (!isset($jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']['payment']['children']['payments-list']['children'][$paymentMethodCode . '-form'])) {
-        continue;
-      }
+			if (!isset($jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']['payment']['children']['payments-list']['children'][$paymentMethodCode . '-form'])) {
+				continue;
+			}
 
-      $billingAddress['street']['children'][0]['label'] = __('Endereço *');
-      $billingAddress['street']['children'][1]['label'] = __('Número *');
-      $billingAddress['street']['children'][2]['label'] = __('Complemento');
-      $billingAddress['street']['children'][3]['label'] = __('Bairro *');
-      $billingAddress['postcode']['sortOrder'] = 1;
+			$billingAddress['street']['children'][0]['label'] = __('Endereço');
+			$billingAddress['street']['children'][1]['label'] = __('Número');
+			$billingAddress['street']['children'][2]['label'] = __('Complemento');
+			$billingAddress['street']['children'][3]['label'] = __('Bairro');
+			$billingAddress['postcode']['sortOrder'] = 1;
 
-      $billingAddress['street']['children']['0']['validation'] = ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255];
-      $billingAddress['street']['children']['1']['validation'] = ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255];
-      $billingAddress['street']['children']['3']['validation'] = ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255];
-    }
+			$billingAddress['street']['children']['0']['validation'] = ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255];
+			$billingAddress['street']['children']['1']['validation'] = ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255];
+			$billingAddress['street']['children']['3']['validation'] = ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255];
+		}
 
-    return $jsLayout;
-  }
+		return $jsLayout;
+	}
 }
